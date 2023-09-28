223405e324dce85e4654fbdc1f535c5fe84b9e86
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-09-29 01:10:54 +0300

    add method for room management

--- client.go
+++ client.go
@@ -45,3 +45,29 @@ type room struct {
 	// holds all current clients in the room
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send the message
+				default:
+					// failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
