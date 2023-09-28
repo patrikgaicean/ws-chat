c3e3fb8a2cf1e81a6def9cff44a43ef15b88c9b2
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-09-29 01:24:33 +0300

    create a room ServeHTTP method

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	// chan that holds incoming messages that should be forwarded to other
 	// clients
@@ -12,28 +19,60 @@ type room struct {
 	clients map[*client]bool
 }
 
-func (r *room) run() {
+func (rm *room) run() {
 	for {
 		select {
-		case client := <-r.join:
+		case client := <-rm.join:
 			// joining
-			r.clients[client] = true
-		case client := <-r.leave:
+			rm.clients[client] = true
+		case client := <-rm.leave:
 			// leaving
-			delete(r.clients, client)
+			delete(rm.clients, client)
 			close(client.send)
-		case msg := <-r.forward:
+		case msg := <-rm.forward:
 			// forward message to all clients
-			for client := range r.clients {
+			for client := range rm.clients {
 				select {
 				case client.send <- msg:
 					// send the message
 				default:
 					// failed to send
-					delete(r.clients, client)
+					delete(rm.clients, client)
 					close(client.send)
 				}
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+func (rm *room) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   rm,
+	}
+	rm.join <- client
+
+	defer func() {
+		rm.leave <- client
+	}()
+
+	go client.write()
+	client.read()
 }
