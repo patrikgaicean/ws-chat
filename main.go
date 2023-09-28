96187f00dbdd4711e13029c23ca91db5ba7504f4
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-09-29 00:40:21 +0300

    add html template and a template handler

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join(
+			"templates",
+			t.filename,
+		)))
+
+		t.templ.Execute(w, nil)
+	})
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
